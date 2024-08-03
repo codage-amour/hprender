bafff1ad908c9a4ae5e7950073a5fd2716ebcf59
unknown <palakverma2003@gmail.com>
2024-08-03 12:45:33 +0530

    fifth

--- static/js/app.js
+++ static/js/app.js
@@ -26,7 +26,7 @@ function getBathValue() {
     var location = document.getElementById("uiLocations");
     var estPrice = document.getElementById("uiEstimatedPrice");
   
- var url = "http://127.0.0.1:5000/./predict_home_price"; 
+ var url = "https://hprender.onrender.com/predict_home_price"; 
 
   
     $.post(url, {
@@ -43,7 +43,7 @@ function getBathValue() {
   
   function onPageLoad() {
     console.log( "document loaded" );
-    var url = "http://127.0.0.1:5000/get_location_names";
+    var url = "https://hprender.onrender.com/get_location_names";
     // var url = "/api/get_location_names"; 
     $.get(url,function(data, status) {
         console.log("got response for get_location_names request");
